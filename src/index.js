e23d81293aed879b8be6e572956f948f355d30d5
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-02 17:19:37 -0400

    commit tree 842135bd6d2793ef6445192f110aed2148463f0b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit',e => {
+        e.preventDefault();
+        const input = document.querySelector('#searchByID');
+        //console.log(e.target.children[1].value);
+        
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('#movieDetails h4');
+            const summary = document.querySelector('#movieDetails p');
+
+            title.innerHTML = data.title;
+            summary.innerHTML = data.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
